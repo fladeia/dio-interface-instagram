f07ec68f8e6d03055c3391564ca220c4e6e4883d
fladeia <ladeiadev@gmail.com>
2022-04-28 19:26:43 -0300

    added image change logic

--- index.js
+++ index.js
@@ -1,92 +1,18 @@
-const image = [...document.querySelectorAll('.image')]
-const image1 = document.querySelector('.image1')
-const image2 = document.querySelector('.image2')
-const image3 = document.querySelector('.image3')
-const image4 = document.querySelector('.image4')
+const image = document.querySelector('.image')
 
-// image.map((item) => {
-//   setInterval(() => {
-//     if(item.classList.contains('show')) {
-//       console.log(item)
-//     }
-//   }, 333000)
-// })
+const array = [
+  'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png', 
+  'https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png', 
+  'https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png',
+  'https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png', 
+]
 
-// setInterval(() => {
-//   if(image1.classList.contains("show")) {
-//     image1.classList.remove("show")
-//   } else {
-//     image1.classList.add("show")
-//   }
-// }, 3000)
-
-// setInterval(() => {
-//   if(image2.classList.contains("show")) {
-//     image2.classList.remove("show")
-//   } else {
-//     image2.classList.add("show")
-//   }
-// }, 3000)
-
-// setInterval(() => {
-//   if(image3.classList.contains("show")) {
-//     image3.classList.remove("show")
-//   } else {
-//     image3.classList.add("show")
-//   }
-// }, 3000)
-
-const imageOne = () => {
-  setTimeout(() => {
-    if(image1.classList.contains("show")) {
-      image1.classList.remove("show")
-    } else {
-      image1.classList.add("show")
-    }
-  }, 3000)
-}
-
-
-
-const imageTwo = () => {
-  setTimeout(() => {
-    if(image2.classList.contains("show")) {
-      image2.classList.remove("show")
-    } else {
-      image2.classList.add("show")
-    }
-  }, 3000)
-}
-
-
-
-const imageThree = () => {
-  setTimeout(() => {
-    if(image3.classList.contains("show")) {
-      image3.classList.remove("show")
-    } else {
-      image3.classList.add("show")
-    }
-  }, 6000)
-}
-
-
-
-const imageFour = () => {
-  setTimeout(() => {
-    if(image4.classList.contains("show")) {
-      image4.classList.remove("show")
-    } else {
-      image4.classList.add("show")
-    }
-  }, 9000)
-}
-
-imageOne()
-imageTwo()
-setTimeout(imageTwo, 3000)
-imageThree()
-setTimeout(imageThree, 3000)
-imageFour()
-
-
+let i = 0
+setInterval(() => {
+  if(i < array.length) {
+    image.setAttribute('src', array[i++])
+  } else {
+    i = 0
+    image.setAttribute('src', array[i++])
+  }
+}, 5000)
